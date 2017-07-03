637ed29849a3e39b490c65244861fdff744f0122
Irving Cabello <irvollo@gmail.com>
2017-07-03 03:26:31 +0000

    Added about page and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
     has_many :runs
     validates :name,    length: {maximum: 20},
                         presence: true
-    validates :mail,    length: {maximum: 30},
+    validates :email,    length: {maximum: 30},
                         presence: true
 end
