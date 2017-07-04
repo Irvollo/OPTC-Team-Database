d0cf5ff7c16ac54ed3a835e1ac781d2b5d7dea34
Irving Cabello <irvollo@gmail.com>
2017-07-04 03:40:35 +0000

    Added partials and bootstrap

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,21 +7,21 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
   
   test "should get home" do
-    get :home
+    get root_path
     assert_response :success
-    assert_select "title", "Home - #{@base_title}"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get :help
+    get help_path
     assert_response :success
-    assert_select "title", "Help - #{@base_title}"
+    assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
-    get :about
+    get about_path
     assert_response :success
-    assert_select "title", "About - #{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
