c703f7afa738590bd19e8f975239225affdbb673
Irving Cabello <irvollo@gmail.com>
2017-07-05 02:56:59 +0000

    Add user validations and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ActiveRecord::Base
+    before_save {email.downcase!}
+    
     has_many :runs
-    validates :name,    length: {maximum: 20},
+    
+    validates :name,    length: {maximum: 50},
                         presence: true
-    validates :email,    length: {maximum: 30},
-                        presence: true
+                        
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email,    length: {maximum: 255},
+                        presence: true,
+                        format: { with:VALID_EMAIL_REGEX}, 
+                        uniqueness: { case_sensitive: false}
+                        
+    has_secure_password 
+    
+    validates :password,    length: { minimum: 6},
+                            presence: true
 end
