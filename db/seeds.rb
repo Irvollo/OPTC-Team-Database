2c50b6820994612ad394f593692271eac53b8856
Irving Cabello <irvollo@gmail.com>
2017-07-13 23:46:30 +0000

    modify seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,3 @@ User.create!(name: "Monkey D. Luffy",
              password_confirmation:"raftel2017",
              admin:"true")
              
-99.times do |n|
-    name = Faker::Name.name
-    email = "example-#{n+1}@railstutorial.org"
-    password = "password"
-    User.create!(name: name,
-                 email: email,
-                 password: password,
-                 password_confirmation: password)
-end
