466004aea669659fe36f947112f1e943d4a8910d
Irving Cabello <irvollo@gmail.com>
2017-07-15 13:44:29 +0000

    Implement reset password mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
    # You can have the root of your site routed with "root"
@@ -19,6 +23,8 @@ Rails.application.routes.draw do
   resources :users
   #Get resources for the email activation
   resources :account_activations, only: [:edit]
+  #Get to create new password resets and updating them
+  resources :password_resets, only: [:new, :create, :edit, :update]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:luffy)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "OPTC Teams - Account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@optcteams.com"], mail.from
     assert_match user.name,               mail.body.encoded
@@ -15,7 +15,7 @@ class UserMailerTest < ActionMailer::TestCase
 
   test "password_reset" do
     mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
+    assert_equal "OPTC Teams - Password reset", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["noreply@optcteams.com"], mail.from
     assert_match "Hi", mail.body.encoded
