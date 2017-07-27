7a350bdf14ec2c94df37d321839d157059eabd2c
Irving Cabello <irvollo@gmail.com>
2017-07-27 21:07:50 +0000

    Search menu filter for level and fight

---
+++ app/assets/javascripts/searches.js
@@ -0,0 +1,16 @@
+jQuery(function() {
+    var fights;
+    fights = $('#search_fight_id').html();
+    console.log(fights);
+    return $('#search_level_id').change(function() {
+      var level, options;
+      level = $('#search_level_id :selected').text();
+      options = $(fights).filter("optgroup[label=" + level + "]").html();
+      console.log(options);
+      if (options) {
+        return $('#search_fight_id').html(options);
+      } else {
+        return $('#search_fight_id').empty();
+      }
+    });
+  });
